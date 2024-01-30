a6daff26e2855ff5d8dc00e105eed6e23a9343ea
kathy2470 <mukisakathy@gmail.com>
2024-01-30 16:50:44 +0300

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,4 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-input = ARGV[0]
-
-regex = /School$/
-
-matches = regex.match(input)
-
-if matches
-  puts matches[0]
-else
-  puts 'No match found'
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
