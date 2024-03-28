90375163cb55ba4c78a57272938d0b4c366da3d4
kathy2470 <mukisakathy@gmail.com>
2024-03-29 00:23:39 +0300

    update

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#This script takes a URL and displays all HTTP methods the server will accept
+
+# Sending an OPTIONS request to the provided URL and displaying the allowed HTTP methods
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
