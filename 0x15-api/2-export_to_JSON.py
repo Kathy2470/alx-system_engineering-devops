ce8f7f897a95201022fc8a771d428864613c210a
kathy2470 <mukisakathy@gmail.com>
2024-04-24 00:23:52 +0300

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
 """
-python script that, using a given REST API, for a given employee ID,
-returns information about his/her TODO list progress and exports it to JSON format.
+Exports to-do list information for a given employee ID to JSON format.
 """
 
-import sys
-import requests
 import json
-
-
-def get_todo_list_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    url_user = f'{base_url}/users/{employee_id}'
-    url_todos = f'{base_url}/todos?userId={employee_id}'
-
-    try:
-        response_user = requests.get(url_user)
-        response_todos = requests.get(url_todos)
-
-        user_data = response_user.json()
-        todos_data = response_todos.json()
-
-        todo_list = [{"task": task['title'], "completed": task['completed'], "username": user_data['username']} for task in todos_data]
-
-        with open(f"{employee_id}.json", mode='w') as file:
-            json.dump({str(user_data['id']): todo_list}, file)
-
-        print(f"Exported data to {employee_id}.json")
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
+import requests
+import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    employee_id = int(sys.argv[1])
-    get_todo_list_progress(employee_id)
+    data_to_export = {
+        user_id: [
+            {"task": t.get("title"), "completed": t.get("completed"), "username": username}
+            for t in todos
+        ]
+    }
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(data_to_export, jsonfile, indent=4)
