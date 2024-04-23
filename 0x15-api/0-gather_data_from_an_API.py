613129935bda4302678191a39c6ec12bb220eb04
kathy2470 <mukisakathy@gmail.com>
2024-04-23 23:18:49 +0300

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,18 +4,12 @@ Python script that, using a given REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
-import requests
 import sys
+import requests
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
+def get_todo_list_progress(employee_id):
+    url_user = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
 
     try:
         response_user = requests.get(url_user)
@@ -27,8 +21,18 @@ if __name__ == "__main__":
         total_tasks = len(todos_data)
         completed_tasks = [task for task in todos_data if task['completed']]
 
-        print("Employee {} is done with tasks({}/{}):".format(user_data['name'], len(completed_tasks), total_tasks))
+        print(f"Employee {user_data['name']} is done with tasks"
+              f"({len(completed_tasks)}/{total_tasks}):")
         for task in completed_tasks:
-            print("\t {}".format(task['title']))
-    except Exception as e:
-        print("Error: {}".format(e))
+            print(f"\t {task['title']}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_todo_list_progress(employee_id)
