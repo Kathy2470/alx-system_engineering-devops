7355c36e037a7b0059be8993349bf91a3b8db408
kathy2470 <mukisakathy@gmail.com>
2024-05-15 00:03:41 +0300

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,20 @@
-# Puppet manifest to configure SSH client to connect without password
+# Puppet manifest to configure SSH client settings
 
-file_line { 'Declare identity file':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[\s#]*IdentityFile',
-  ensure             => present,
+# Ensure the SSH configuration file exists
+file { '/etc/ssh/ssh_config':
+  ensure => present,
 }
 
+# Configure SSH to use the private key ~/.ssh/school
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '  IdentityFile ~/.ssh/school',
+  match => '^.*IdentityFile.*$',
+}
+
+# Configure SSH to refuse password authentication
 file_line { 'Turn off passwd auth':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^[\s#]*PasswordAuthentication',
-  ensure             => present,
+  path  => '/etc/ssh/ssh_config',
+  line  => '  PasswordAuthentication no',
+  match => '^.*PasswordAuthentication.*$',
 }
