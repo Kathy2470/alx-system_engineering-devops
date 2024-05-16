28d87d0946136ff015becf30e5bb3ebcc4064544
kathy2470 <mukisakathy@gmail.com>
2024-05-17 01:01:16 +0300

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,17 +6,13 @@ exec { 'fix-ulimit-for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   # Specify the path for the sed command
   path    => '/usr/local/bin:/bin:/usr/bin',
-  # Ensure this command is executed as root
   user    => 'root',
-  # Notify the nginx-restart exec to run if this command changes the file
   notify  => Exec['nginx-restart'],
 }
 
 # Restart nginx
 exec { 'nginx-restart':
-  # Restart nginx service
   command => '/etc/init.d/nginx restart',
-  # Specify the path for init.d script
   path    => '/sbin:/bin:/usr/sbin:/usr/bin',
   # Only run this command if notified by fix-ulimit-for-nginx
   refreshonly => true,
