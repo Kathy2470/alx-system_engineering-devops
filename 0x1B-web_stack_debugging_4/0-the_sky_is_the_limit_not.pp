ca4c100e029438372d36339c48b403736429da20
kathy2470 <mukisakathy@gmail.com>
2024-05-17 00:22:21 +0300

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,23 @@
-# This Puppet manifest is intended to fix issues causing failed requests on the web server
+# Increase the amount of traffic an nginx server can handle
 
-# Run a command to fix the Nginx configuration
-exec { 'fix--for-nginx':
-  command     => 'echo "Nginx configuration fixed"',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  notify      => Service['nginx'],
+# Increase the ULIMIT of the default file
+exec { 'fix-ulimit-for-nginx':
+  # Modify the ULIMIT value
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  # Specify the path for the sed command
+  path    => '/usr/local/bin:/bin:/usr/bin',
+  # Ensure this command is executed as root
+  user    => 'root',
+  # Notify the nginx-restart exec to run if this command changes the file
+  notify  => Exec['nginx-restart'],
 }
 
-# Restart Nginx service to apply configuration changes
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+# Restart nginx
+exec { 'nginx-restart':
+  # Restart nginx service
+  command => '/etc/init.d/nginx restart',
+  # Specify the path for init.d script
+  path    => '/sbin:/bin:/usr/sbin:/usr/bin',
+  # Only run this command if notified by fix-ulimit-for-nginx
+  refreshonly => true,
 }
